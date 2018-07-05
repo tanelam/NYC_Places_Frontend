bbfe0a5e850d2151883b13e56f514b594d18af79
tanelam <tania.e.aparicio@gmail.com>
2018-07-05 18:16:40 -0400

    turned class component into functional component

--- src/Components/FoodCard.js
+++ src/Components/FoodCard.js
@@ -1,29 +1,22 @@
 import React, { Fragment } from "react"
 import image from "./eventImage.png"
 
-class FoodCard extends React.Component{
-
-  render(){
-     // console.log(this.props.food === [])
-    return(
-    <Fragment>
-      <div className="card">
-        <a className="image" href={this.props.food.url} target="_blank">
-          {this.props.food.image_url ?
-            <img src={this.props.food.image_url} alt="" /> :
-            <img src={image} alt="" />}
-        </a>
-        <div className="content">
-          <a className="ui grey header" href={this.props.food.url} target="_blank">{this.props.food.name}</a>
-          <div className="description">
-            {this.props.food.location.display_address}
-          </div>
+const FoodCard = (props) => (
+  <Fragment>
+    <div className="card">
+      <a className="image" href={props.food.url} target="_blank">
+        {props.food.image_url ?
+          <img src={props.food.image_url} alt="" /> :
+          <img src={image} alt="" />}
+      </a>
+      <div className="content">
+        <a className="ui grey header" href={props.food.url} target="_blank">{props.food.name}</a>
+        <div className="description">
+          {props.food.location.display_address}
         </div>
       </div>
-    </Fragment>
-    )
-  }
-}
-
+    </div>
+  </Fragment>
+)
 
 export default FoodCard
