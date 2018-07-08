5c692d2ded96ae029f4896305b1213f809b2fa22
tanelam <tania.e.aparicio@gmail.com>
2018-07-08 18:12:08 -0400

    change the name of timestamp variable

--- src/utils/yelpApiFetcher.js
+++ src/utils/yelpApiFetcher.js
@@ -4,11 +4,11 @@ export const corsURL = "https://cors-anywhere.herokuapp.com/"
 export const baseURL = "https://api.yelp.com/v3/"
 
 //EventsTimeStamp
-const dateTimeStamp = Math.round(new Date().getTime()/1000.0)
+const TimeStamp = Math.round(new Date().getTime()/1000.0)
 
 //EventsURL's
 export const EventsLimitAndRadius = "events?limit=4&radius=2&location="
-export const EventsDateAndCategories = `&start_date=${dateTimeStamp}&end_date=${dateTimeStamp}&categories=music,visual-arts,fashion,food-and-drink,festivals-fairs,kids-family`
+export const EventsDateAndCategories = `&start_date=${TimeStamp}&end_date=${TimeStamp}&categories=music,visual-arts,fashion,food-and-drink,festivals-fairs,kids-family`
 
 //FoodPlacesURL's
 export const FoodPlacesLocation = "businesses/search?location="
